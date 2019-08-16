ef152bf987d47a4c4ca5aa429edf115875d55396
Bruno <Bruno@192.168.1.4>
2019-08-15 21:10:06 -0300

    Adição de testes TextField e TextArea.

--- cursoSelenium/src/main/java/cursoSelenium/TesteGoogle.java
+++ cursoSelenium/src/main/java/cursoSelenium/TesteGoogle.java
@@ -4,14 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class TesteGoogle {
 	
 	@Test
 	public void teste() {
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Bruno\\drivers\\FirefoxDriver\\geckodriver.exe");	    
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bruno\\drivers\\ChromeDriver\\chromedriver.exe");	    
+		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com.br");
 		Assert.assertEquals("Google", driver.getTitle());
 		System.out.println(driver.getTitle());
